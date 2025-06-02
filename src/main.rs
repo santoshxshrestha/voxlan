bc6b2032ed8cb71dffa9080ea1759982bfcba389
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-02 16:33:17 +0545

    added the ip and port

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::{net::TcpStream, thread, time::Duration};
 
+use std::net::UdpSocket;
+
+fn get_local_ip() -> std::io::Result<std::net::IpAddr> {
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+    socket.connect("8.8.8.8:80")?;
+    Ok(socket.local_addr()?.ip())
+}
+
 fn scan_port(ip: &str, port: u16) {
     let addr = format!("{}:{}", ip, port);
     if TcpStream::connect_timeout(&addr.parse().unwrap(), Duration::from_millis(100)).is_ok() {
@@ -8,6 +16,10 @@ fn scan_port(ip: &str, port: u16) {
 }
 
 fn main() {
+    match get_local_ip() {
+        Ok(ip) => println!("Got you ip {ip}"),
+        Err(e) => eprint!("Error couldn't get you ip : {}", e),
+    }
     let ip = "127.0.0.1";
     let mut handles = vec![];
 
