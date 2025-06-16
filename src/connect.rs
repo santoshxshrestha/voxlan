9972858f62a35b093241c2f04fe8c3042fdf0c4b
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-17 00:57:05 +0545

    finally made it work with the spawn_blocking of hte tokio

--- src/connect.rs
+++ src/connect.rs
@@ -2,32 +2,31 @@
 use std::error::Error;
 use std::io;
 use std::io::Write;
-
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 
 pub async fn handle_write(mut owned_write_half: OwnedWriteHalf) -> io::Result<()> {
     loop {
-        print!("Message 󰁕 ");
-        io::stdout().flush()?;
+        let input = tokio::task::spawn_blocking(|| {
+            print!("Message 󰁕 ");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            input
+        })
+        .await
+        .unwrap();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
         let message = input.trim();
-
         if message == "quit" || message == "q" || message == "exit" {
             println!("󱠡 Goodbye!");
             break;
         }
-
         if message.is_empty() {
             continue;
         }
-
-        owned_write_half
-            .write_all((format!("{}", message)).as_bytes())
-            .await?;
+        owned_write_half.write_all(message.as_bytes()).await?;
     }
     Ok(())
 }
@@ -37,20 +36,18 @@ pub async fn handle_read(mut owned_read_half: OwnedReadHalf) -> io::Result<()> {
         let mut buffer = vec![0; 1024];
         match owned_read_half.read(&mut buffer).await {
             Ok(0) => {
-                println!("Client Disconnected gracefully");
-                break; // Connection closed
+                println!("Server Disconnected gracefully");
+                break;
             }
             Ok(n) => buffer.truncate(n),
             Err(e) if e.kind() == io::ErrorKind::ConnectionReset => {
-                println!("Client disconnected abruptly (reset)");
-                break; // loop gets exited
+                println!("Server disconnected abruptly (reset)");
+                break;
             }
             Err(e) => {
                 println!("Failed reading stream:{}", e);
-                // return Err(Box::new(e));
             }
         }
-
         let message = String::from_utf8_lossy(&buffer[..]);
         println!("Received message: {}", message.trim());
     }
@@ -58,19 +55,19 @@ pub async fn handle_read(mut owned_read_half: OwnedReadHalf) -> io::Result<()> {
 }
 
 pub async fn connect(target_port: u16, ip: String) -> Result<(), std::io::Error> {
-    loop {
-        let mut stream = TcpStream::connect(format!("{}:{}", ip, target_port)).await?;
-        let (owned_read_half, owned_write_half) = stream.into_split();
+    let stream = TcpStream::connect(format!("{}:{}", ip, target_port)).await?;
+    println!("Connected to server {}:{}", ip, target_port);
 
-        tokio::spawn(async move {
-            let (r, w) = tokio::join!(handle_read(owned_read_half), handle_write(owned_write_half));
-            if let Err(e) = r {
-                eprintln!("Read error: {}", e);
-            }
-            if let Err(e) = w {
-                eprintln!("Write error: {}", e);
-            }
-        });
+    let (owned_read_half, owned_write_half) = stream.into_split();
+
+    let (r, w) = tokio::join!(handle_read(owned_read_half), handle_write(owned_write_half));
+
+    if let Err(e) = r {
+        eprintln!("Read error: {}", e);
     }
+    if let Err(e) = w {
+        eprintln!("Write error: {}", e);
+    }
+
     Ok(())
 }
--- src/host.rs
+++ src/host.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+use crate::show_pulsing;
 use std::error::Error;
 use std::io;
 use std::io::Write;
@@ -6,28 +7,32 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 
-use crate::animation::show_pulsing;
 pub async fn handle_write(mut owned_write_half: OwnedWriteHalf) -> io::Result<()> {
     loop {
-        print!("Message 󰁕 ");
-        io::stdout().flush()?;
+        // These will work fine but   io::stdin().read_line(&mut input)?;  will block the fn so,
+        // not to use it
+        // let stdin = tokio::io::stdin();
+        // let reader = BufReader::new(stdin);
+        // let mut lines = reader.lines();
+        let input = tokio::task::spawn_blocking(|| {
+            print!("Message 󰁕 ");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            input
+        })
+        .await
+        .unwrap();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
         let message = input.trim();
-
         if message == "quit" || message == "q" || message == "exit" {
             println!("󱠡 Goodbye!");
             break;
         }
-
         if message.is_empty() {
             continue;
         }
-
-        owned_write_half
-            .write_all((format!("{}", message)).as_bytes())
-            .await?;
+        owned_write_half.write_all(message.as_bytes()).await?;
     }
     Ok(())
 }
@@ -38,19 +43,17 @@ pub async fn handle_read(mut owned_read_half: OwnedReadHalf) -> io::Result<()> {
         match owned_read_half.read(&mut buffer).await {
             Ok(0) => {
                 println!("Client Disconnected gracefully");
-                break; // Connection closed
+                break;
             }
             Ok(n) => buffer.truncate(n),
             Err(e) if e.kind() == io::ErrorKind::ConnectionReset => {
                 println!("Client disconnected abruptly (reset)");
-                break; // loop gets exited
+                break;
             }
             Err(e) => {
                 println!("Failed reading stream:{}", e);
-                // return Err(Box::new(e));
             }
         }
-
         let message = String::from_utf8_lossy(&buffer[..]);
         println!("Received message: {}", message.trim());
     }
@@ -60,12 +63,13 @@ pub async fn handle_read(mut owned_read_half: OwnedReadHalf) -> io::Result<()> {
 pub async fn host(bind_port: u16, local_ip: String) -> io::Result<()> {
     let listener = TcpListener::bind(format!("0.0.0.0:{}", bind_port)).await?;
     show_pulsing();
+    // println!("Server listening on port {}", bind_port);
     println!(
         "connect to this server by following command \n voxlan connect -i {} -t {}",
         local_ip, bind_port
     );
+
     loop {
-        //here the stream and the add are the socket and the ip of the connnected thinge
         let (stream, addr) = listener.accept().await?;
         println!("New connection {}", addr);
         let (owned_read_half, owned_write_half) = stream.into_split();
@@ -80,5 +84,4 @@ pub async fn host(bind_port: u16, local_ip: String) -> io::Result<()> {
             }
         });
     }
-    Ok(())
 }
